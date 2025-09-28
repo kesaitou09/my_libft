cf6f18122a36717189bd991853ec14e97ee8fb42
ksaitou <ksaitou@ksaitounoMacBook-Air.local>
2025-09-28 09:11:59 +0900

    libft-init4

--- libft/srcs/strlcat.c
+++ libft/srcs/strlcat.c
@@ -1,4 +1,50 @@
 #include "libft.h"
 
-// 実装: strlcat
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+	size_t	j;
 
+	src_len = strlen(src);
+	i = 0;
+	while (dst[i])
+		i++;
+	dst_len = i;
+	j = 0;
+	if (size > dst_len)
+	{
+		while (j < size - 1)
+			dst[i++] = src[j++];
+		dst[i] = '\0';
+	}
+	return (dst_len + src_len);
+}
+
+int	main(void)
+{
+	char buf1[] = "aaa";
+	char buf2[] = "jjkkkk";
+	char buf3[] = "aaaaaaaaaa";
+
+	char dst1[] = "dst1dayo";
+	char dst2[] = "dst2dayo";
+	char dst3[10];
+	char dst4[10];
+	char dst5[10];
+
+	printf("dst1  %zu\n%s\n", ft_strlcat(dst1, buf1, sizeof(dst1)), dst1);
+	printf("dst2  %zu\n%s\n", strlcat(dst2, buf1, sizeof(dst2)), dst2);
+	//printf("dst3  %zu\n%s\n", ft_strlcpy(dst3, buf3, sizeof(dst3)), dst3);
+	//printf("dst4  %zu\n%s\n", strlcpy(dst4, buf3, sizeof(dst4)), dst4);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- libft/srcs/strnstr.c
+++ libft/srcs/strnstr.c
@@ -1,4 +1,42 @@
 #include "libft.h"
 
-// 実装: strnstr
+// ヌル文字になるまで ＆＆サイズの回数だけ 探索文字列が出現するかを調べる。第二引数の終端まで一致してる場合のみその部分文字列の先頭を返す。
+// 第二引数のn文字目までが存在するかどうかではない。
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	const char	*s1;
+	const char	*s2;
+	const char	*punc;
+	size_t		i;
 
+	i = 0;
+	if (!*little)
+		return (char *)(big);
+	(s1 = big, s2 = little, punc = NULL);
+	while (*s1 && i < len)
+	{
+		if (*s1 == *s2)
+		{
+			punc = s1;
+			(s1++, s2++, i++);
+			while (*s1 && i < len && (*s1 == *s2))
+				(s1++, s2++, i++);
+			if (!*s2)
+				return (char *)(punc);
+			s2 = little;
+		}
+		else
+			(s1++, i++);
+	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	printf("libc %s\n", strnstr("abcdefhfiaefj", NULL, 30));
+	printf("my %s\n", ft_strnstr("abcdefhfiaefj", NULL, 30));
+	// printf("%s\n",strnstr("abcdefhijk","de",3));
+	// printf("%s\n",strnstr("abcdefhijk","de",3));
+	// printf("%s\n",strnstr("abcdefhijk","de",3));
+	// printf("%s\n",strnstr("abcdefhijk","de",3));
+}
